eb08f3e46cc80b93a8b6ce70c5f26e2afe9078b7
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2022-12-23 11:25:58 +0200

    Updated tsconfig and package.json in lib

--- dist/ngx-flowchart/lib/node-dragging.service.d.ts
+++ dist/ngx-flowchart/lib/node-dragging.service.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="jquery" />
+/// <reference types="jquery" />
 import { FcModelService } from './model.service';
 import { FcNode } from './ngx-flowchart.models';
 export declare class FcNodeDraggingService {
--- dist/ngx-flowchart/ngx-flowchart.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="ngx-flowchart" />
-export * from './public-api';
